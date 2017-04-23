2cb993eb099411a21e50e2bf09f1cf24b450b40a
nkj4098 <nishantkumarjha40@gmail.com>
2017-04-23 15:40:49 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+var myheading = document.querySelector('h1');
+myheading.textContent = 'Mozilla Firefox';
+
+var myvariable ="nishant";
+myvariable;
+
+/*
+Everything in between is a comment.
+*/
+
+// This is a comment
+
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/logo.png') {
+      myImage.setAttribute ('src','images/logodev.png');
+    } else {
+      myImage.setAttribute ('src','images/logo.png');
+    }
+}
+
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+  var myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Mozilla is cool, ' + myName;
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Mozilla is cool, ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
